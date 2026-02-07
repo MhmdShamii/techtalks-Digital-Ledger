f82333473bd2c7ec656b1d40aad86a8364702dc5
Mohammad AbouAlHasan <mhmd79141097@gmail.com>
2026-02-07 12:09:06 +0200

    Create Customer model and link to Store

---
+++ app/Models/Customer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'store_id',
+        'name',
+        'phone',
+        'email',
+        'is_active',
+    ];
+
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class);
+    }
+}
